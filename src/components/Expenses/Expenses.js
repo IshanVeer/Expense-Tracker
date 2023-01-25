4152b8e8b6cd7cde0356ae75d19c6f6b36f26d6f
Ishan Veer <ishanpveer@gmail.com>
2023-01-25 11:26:24 +0530

    added error handle

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,23 @@ const Expenses = (props) => {
   const selectedYearHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear; //filtering the data according to the year of that expense.
+  });
+
+  let filteredContent = <p style={{color:"white", textAlign:"center", fontSize:"2rem"}}>No Items Found</p>;
+//To send an error message if the expenses item list is empty for that year. 
+  if (filteredExpenses.length > 0) {
+    filteredContent=filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title} //Transforming our array into JSX element using map method
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Fragment>
       <Card className={classes["expenses"]}>
@@ -18,18 +35,7 @@ const Expenses = (props) => {
           onSelectYear={selectedYearHandler}
           selected={filteredYear}
         />
-        {props.expenses.map(
-          (
-            expense // Used map method to transform the data from array into JSX element.
-          ) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          )
-        )}
+        {filteredContent}
       </Card>
     </Fragment>
   );
