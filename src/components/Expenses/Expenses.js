af1dab71b31214389e6362b7dd19221c0d725565
Ishan Veer <ishanpveer@gmail.com>
2023-01-26 09:45:45 +0530

    added chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,16 @@
 import React, { useState, Fragment } from "react";
 import ExpenseItem from "./ExpenseItem";
 
+
 import classes from "./Expenses.module.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesChart from "./ExpenseChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
 
-  const selectedYearHandler = (selectedYear) => {
+  const selectedYearHandler = (selectedYear) => { // To have the current year as the default year and still be able to select other years
     setFilteredYear(selectedYear);
   };
 
@@ -16,10 +18,14 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear; //filtering the data according to the year of that expense.
   });
 
-  let filteredContent = <p style={{color:"white", textAlign:"center", fontSize:"2rem"}}>No Items Found</p>;
-//To send an error message if the expenses item list is empty for that year. 
+  let filteredContent = (
+    <p style={{ color: "white", textAlign: "center", fontSize: "2rem" }}>
+      No Items Found
+    </p>
+  );
+  //To send an error message if the expenses item list is empty for that year.
   if (filteredExpenses.length > 0) {
-    filteredContent=filteredExpenses.map((expense) => (
+    filteredContent = filteredExpenses.map((expense) => (
       <ExpenseItem
         key={expense.id}
         title={expense.title} //Transforming our array into JSX element using map method
@@ -35,6 +41,7 @@ const Expenses = (props) => {
           onSelectYear={selectedYearHandler}
           selected={filteredYear}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         {filteredContent}
       </Card>
     </Fragment>
